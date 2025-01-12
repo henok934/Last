c32d0e6e06395c35463b02975370e5712937119c
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2025-01-12 12:05:56 -0500

    host

--- myproje/wsgi.py
+++ myproje/wsgi.py
@@ -1,25 +1,5 @@
-"""
-WSGI config for my_django_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
-"""
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'wsgi' command.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users.settings')
-
-# Get the WSGI application.
-application = get_wsgi_application()
-"""
-
-
-
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproje.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 application = get_wsgi_application()
