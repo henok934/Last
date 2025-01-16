48f0708438c9c0670fc46d61c36e1e16c5e78ff7
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2025-01-16 14:31:22 -0500

    bus ticket

--- myproje/venv/bin/activate.fish
+++ myproje/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/henok/H/Last/myproje/venv
+set -gx VIRTUAL_ENV /home/henok/Last/myproje/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
