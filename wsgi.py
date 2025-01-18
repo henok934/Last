841b8e2971c272c844a81a0715ef2291d90c8738
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2025-01-18 10:27:23 -0500

    fixe

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
-<<<<<<< HEAD
 # Set the default settings module for the 'wsgi' command.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproje.settings')
 
 # Get the WSGI application.
-=======
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users.settings')
->>>>>>> origin/main
 application = get_wsgi_application()
