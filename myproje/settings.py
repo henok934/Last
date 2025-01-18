1c5635dc8140f899bf39b76c3f879c14e83e4150
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2025-01-18 07:46:38 -0500

    hosts

--- myproje/settings.py
+++ myproje/settings.py
@@ -471,6 +471,7 @@ MIDDLEWARE = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
